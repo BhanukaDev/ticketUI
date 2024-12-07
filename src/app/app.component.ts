1404ddfc6fd930406e8f1e02915ea2fab55436a4
ifs-bhdalk <bhanuka.dassanayake@ifs.com>
2024-12-07 08:58:07 +0530

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'TicketingSimulation';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { ViewSimComponent } from './components/view-sim/view-sim.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainMenuComponent,
+  },
+  {
+    path: 'view-sim',
+    component: ViewSimComponent,
+  },
+];
